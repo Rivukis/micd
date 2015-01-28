00289d3b9bd6c42ebd37c47a970f8ed330346409
Timothy Hise <Timelord@Timothys-MacBook-Pro.local>
2015-01-28 15:39:17 -0800

    Initial Commit

---
+++ micd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  micd
+//
+//  Created by Timothy Hise on 1/28/15.
+//  Copyright (c) 2015 CleverKnot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
