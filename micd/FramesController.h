536bbb2a6a959d742d6bf1a8bcd55353f036f43f
Timelord <fraglerocked@gmail.com>
2015-02-16 13:41:26 -0800

    A Good Place

--- micd/FramesController.h
+++ micd/FramesController.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <CoreGraphics/CoreGraphics.h>
+#import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSUInteger, HomeViewContollerPositionState) {
     HomeViewContollerPositionStateRecordings = 0,
