ec0118650ce8288c414285b655a08b20d5cff894
Brian Radebaugh <RadebaughBrian@gmail.com>
2015-02-16 13:22:02 -0800

    added FramesController class to project

---
+++ micd/FramesController.m
@@ -0,0 +1,55 @@
+//
+//  CustomFrames.m
+//  micd
+//
+//  Created by Brian Radebaugh on 2/15/15.
+//  Copyright (c) 2015 CleverKnot. All rights reserved.
+//
+
+#import "FramesController.h"
+
+@implementation FramesController
+
++ (CGRect)adjustedFrameBasedOnHeightFromOriginalFrame:(CGRect)originalFrame {
+    CGRect currentScreenBounds = [FramesController currentScreenBounds];
+    CGRect designedForScreenBounds = [FramesController screenDesignedForBounds];
+    double currentScreenToDesignedForScreenHeightsRatio = currentScreenBounds.size.height / designedForScreenBounds.size.height;
+    
+    CGFloat adjustedWidth = originalFrame.size.width * currentScreenToDesignedForScreenHeightsRatio;
+    CGFloat adjustedHeight = originalFrame.size.height * currentScreenToDesignedForScreenHeightsRatio;
+    
+    CGFloat originCenterX = originalFrame.origin.x + originalFrame.size.width/2.0f;
+    CGFloat adjustedCenterX = originCenterX * currentScreenToDesignedForScreenHeightsRatio;
+    
+    CGFloat adjustedX = adjustedCenterX - adjustedWidth/2.0f;
+    CGFloat adjustedY = originalFrame.origin.y * currentScreenToDesignedForScreenHeightsRatio;
+    
+    return CGRectMake(adjustedX, adjustedY, adjustedWidth, adjustedHeight);
+}
+
++ (CGRect)adjustedFrameBasedOnWidthFromOriginalFrame:(CGRect)originalFrame {
+    CGRect currentScreenBounds = [FramesController currentScreenBounds];
+    CGRect designedForScreenBounds = [FramesController screenDesignedForBounds];
+    double currentScreenToDesignedForScreenWidthsRatio = currentScreenBounds.size.width / designedForScreenBounds.size.width;
+    
+    CGFloat adjustedWidth = originalFrame.size.width * currentScreenToDesignedForScreenWidthsRatio;
+    CGFloat adjustedHeight = originalFrame.size.height * currentScreenToDesignedForScreenWidthsRatio;
+    
+    CGFloat originCenterY = originalFrame.origin.y + originalFrame.size.height/2.0f;
+    CGFloat adjustedCenterY = originCenterY * currentScreenToDesignedForScreenWidthsRatio;
+    
+    CGFloat adjustedX = originalFrame.origin.x * currentScreenToDesignedForScreenWidthsRatio;
+    CGFloat adjustedY = adjustedCenterY - adjustedHeight/2.0f;
+    
+    return CGRectMake(adjustedX, adjustedY, adjustedWidth, adjustedHeight);
+}
+
++ (CGRect)currentScreenBounds {
+    return [[UIScreen mainScreen] bounds];
+}
+
++ (CGRect)screenDesignedForBounds {
+    return CGRectMake(0, 0, 375, 667);
+}
+
+@end
