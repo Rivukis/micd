06234c38b04384a3db83be9a632f5393b66f7a37
Timelord <fraglerocked@gmail.com>
2015-02-27 16:57:26 -0800

    added recordingsSection model

---
+++ micd/RecordingsSection.h
@@ -0,0 +1,23 @@
+//
+//  RecordingsSection.h
+//  micd
+//
+//  Created by Timothy Hise on 2/27/15.
+//  Copyright (c) 2015 CleverKnot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Recording;
+
+@interface RecordingsSection : NSObject
+
+@property (nonatomic, readonly) NSInteger numberOfRecordings;
+
++ (NSArray *)arrayOfSectionsForRecordings:(NSArray *)recordings;
+
+- (Recording *)recordingAtIndex:(NSInteger)index;
+
+- (NSString *)dateAsString;
+
+@end
---
+++ micd/RecordingsSection.m
@@ -0,0 +1,14 @@
+//
+//  RecordingsSection.m
+//  micd
+//
+//  Created by Timothy Hise on 2/27/15.
+//  Copyright (c) 2015 CleverKnot. All rights reserved.
+//
+
+#import "RecordingsSection.h"
+
+@implementation RecordingsSection
+
+@end
+
