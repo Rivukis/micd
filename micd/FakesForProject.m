edb2d99abce0d42606e5b38d2af81bbd6d2866f8
Brian Radebaugh <rivukis@gmail.com>
2015-03-21 19:58:38 -0700

    recordings sections now list each day of the week separately if within a week of the now, fixed issue when using fake data, added new fake entries

--- micd/FakesForProject.m
+++ micd/FakesForProject.m
@@ -14,10 +14,16 @@
 + (NSArray *)fakeArrayOfSearchItems {
     
     NSDate *today = [NSDate date];
+    NSDate *yesterday = [NSDate dateWithTimeInterval:-60*60*24 sinceDate:today];
+    NSDate *threeDaysAgo = [NSDate dateWithTimeInterval:-60*60*24*3 sinceDate:today];
+    NSDate *fourDaysAgo = [NSDate dateWithTimeInterval:-60*60*24*4 sinceDate:today];
+    NSDate *sixDaysAgo = [NSDate dateWithTimeInterval:-60*60*24*6 sinceDate:today];
+    NSDate *eightDaysAgo = [NSDate dateWithTimeInterval:-60*60*24*8 sinceDate:today];
+    NSDate *nineDaysAgo = [NSDate dateWithTimeInterval:-60*60*24*9 sinceDate:today];
     NSDate *lastMonth = [NSDate dateWithTimeInterval:-2628000  sinceDate:today];
     NSDate *twoMonthsAgo = [NSDate dateWithTimeInterval:-2628000*2  sinceDate:today];
     NSDate *fourMonthsAgo = [NSDate dateWithTimeInterval:-2628000*4  sinceDate:today];
-    NSInteger variety = 0;
+    NSInteger variety = 10;
     
     Recording *recording1 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:today] length:300 name:@"song title 1"];
     Recording *recording2 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:lastMonth] length:220 name:@"sonetitle2"];
@@ -43,17 +49,25 @@
     Recording *recording24 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:twoMonthsAgo] length:378 name:@"Really long title to see what happens with the wrapping"];
     Recording *recording25 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:twoMonthsAgo] length:301 name:@"Confrontation with pops"];
     Recording *recording26 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:fourMonthsAgo] length:1205 name:@"My baby's first words"];
-    variety += 86400;
+//    variety += 86400;
     Recording *recording27 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:fourMonthsAgo] length:74 name:@"Massive fart"];
     Recording *recording28 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:fourMonthsAgo] length:843 name:@"Fetivus of the bestivus"];
     Recording *recording29 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:fourMonthsAgo] length:378 name:@"Really long title to see what happens with the wrapping"];
+    Recording *recording30 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:yesterday] length:301 name:@"yesterday title"];
+    Recording *recording31 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:threeDaysAgo] length:1205 name:@"three days ago title - wed"];
+    Recording *recording32 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:threeDaysAgo] length:74 name:@"three days ago title 2 - wed"];
+    Recording *recording33 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:fourDaysAgo] length:843 name:@"four days ago title - tue"];
+    Recording *recording34 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:sixDaysAgo] length:378 name:@"six days ago - sun"];
+    Recording *recording35 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:eightDaysAgo] length:74 name:@"eight days ago"];
+    Recording *recording36 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:eightDaysAgo] length:843 name:@"eight days ago again"];
+    Recording *recording37 = [[Recording alloc] initWithData:nil date:[NSDate dateWithTimeInterval:variety-- sinceDate:nineDaysAgo] length:378 name:@"nine days ago"];
     
     [recording1 addTag:@"tag"];
     [recording2 addTag:@"title"];
     [recording2 addTag:@"song"];
     [recording8 addTag:@"kitty"];
     
-    return @[recording1, recording2, recording3, recording4, recording5, recording6, recording7, recording8, recording9, recording10, recording11, recording13, recording14, recording15, recording16, recording17, recording18, recording19, recording20, recording21, recording23, recording24, recording25, recording26, recording27, recording28, recording29];
+    return @[recording1, recording2, recording3, recording4, recording5, recording6, recording7, recording8, recording9, recording10, recording11, recording13, recording14, recording15, recording16, recording17, recording18, recording19, recording20, recording21, recording23, recording24, recording25, recording26, recording27, recording28, recording29, recording30, recording31, recording32, recording33, recording34, recording35, recording36, recording37];
 }
 
 @end
